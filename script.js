208119349d03fd325a5fda015eea2a3dbfbee1b0
Sushil Athreya <sushilathreya@gmail.com>
2024-07-02 14:25:35 +0530

    Added custom cursor & basic responsiveness

---
+++ script.js
@@ -0,0 +1,8 @@
+    function toggleMenu() {
+        var navLinks = document.getElementById("nav-links");
+        if (navLinks.classList.contains("show")) {
+            navLinks.classList.remove("show");
+        } else {
+            navLinks.classList.add("show");
+        }
+    }
