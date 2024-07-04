da9b69007a073a1616181bfbc52d3fb8f4420fb1
Sushil Athreya <sushilathreya@gmail.com>
2024-07-04 14:36:26 +0530

    Changed font and added tooltips on hover

--- script.js
+++ script.js
@@ -6,3 +6,23 @@
             navLinks.classList.add("show");
         }
     }
+
+// For Tooltip positioning
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tooltip = document.getElementById('tooltip');
+
+    document.querySelectorAll('.hover-img').forEach(item => {
+        item.addEventListener('mousemove', (e) => {
+            const tooltipText = item.getAttribute('data-tooltip');
+            tooltip.textContent = tooltipText;
+            tooltip.style.left = `${e.pageX + 10}px`; // Adjust the position of the tooltip
+            tooltip.style.top = `${e.pageY + 10}px`;
+            tooltip.style.visibility = 'visible';
+        });
+
+        item.addEventListener('mouseleave', () => {
+            tooltip.style.visibility = 'hidden';
+        });
+    });
+});
