749a1cfaa9cc2a8d8643207dcd26eda78eb640a6
Sushil Athreya <sushilathreya@gmail.com>
2024-10-22 00:43:54 +0530

    Added clickable images, market, and ICP

--- script.js
+++ script.js
@@ -50,5 +50,18 @@ function filterProjects(tag) {
                 project.style.display = 'none';
             }
         }
-    });
+    });  
+}
+
+function openLightbox(image) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    
+    lightbox.style.display = 'flex';
+    lightboxImg.src = image.src;
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
 }
